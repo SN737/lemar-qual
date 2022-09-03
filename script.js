709bb5d75c4651459af7c92e6d87508f6b1b7723
Sergey <92508690+SN737@users.noreply.github.com>
2022-09-03 12:50:00 +0300

    update

--- script.js
+++ script.js
@@ -3,19 +3,26 @@ const inputMain = document.querySelector('.input0')
 const addBtn = document.querySelector('.addBtn');
 const driverList = document.querySelector('.driverlist');
 const sortBtn = document.querySelector('.sortBtn');
+const participants = document.querySelector('.participants');
 
-addBtn.addEventListener('click', ()=>{
+let partValue = participants.value;
+
+addBtn.addEventListener('click', addDriver);
+inputMain.addEventListener('keydown', (e)=>{
+
+    // alert(e.code)
+    if(e.code !=='Enter'){
+        return
+    }else addDriver()
+})
+
+function addDriver (){
     let  id = (Math.random()*Math.random() * 1000);
     const driver = new Driver(inputMain.value, id)
     driverArray.push(driver)
     displayDrivers(driver.name, driver.id)
     inputMain.value = ''
-
-    //пока не нужно перебирать
-    // driverArray.forEach((obj)=> {
-    //     displayDrivers(obj.name)
-    // })
-});
+}
 
 sortBtn.addEventListener('click', sortDrivers)
 
@@ -44,12 +51,23 @@ function displayDrivers(name, id) {
 function sortDrivers() {
     const id = 'id';
     const sortedArr = driverArray.sort((id1, id2)=>id1[id]>id2[id] ? 1 : -1);
-    sortedArr.forEach((obj) => {
-        displaySorted(obj.name)
-    })
+    // sortedArr.forEach((obj) => {
+    //     displaySorted(obj.name)
+    // })
+    groupingDrivers(sortedArr, partValue)
 
 }
-    function displaySorted(name) {
+
+function groupingDrivers(sortedArr, partValue){
+    let groupedArr = sortedArr.splice(0,partValue);
+
+    groupedArr.forEach((obj) => {
+        displaySorted(obj.name)
+    });
+}
+
+
+function displaySorted(name) {
    const driverItemSorted = document.createElement('div');
    driverItemSorted.classList.add('driverItemSorted');
    const sortedlist = document.querySelector('.sortedlist')
